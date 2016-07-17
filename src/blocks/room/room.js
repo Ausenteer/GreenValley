809ab0add945a1c14d4acfc52008f2a217533482
Ausenteer <elena.schykina@gmail.com>
2016-07-17 18:30:14 +0300

    blocks room done

---
+++ src/blocks/room/room.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  // Находим в ДОМе элементы для карусели
+  var carousel_1 = $('#slider-room');
+
+  // Инициируем карусели
+  carousel_1.owlCarousel({ items: 1 });
+
+});
